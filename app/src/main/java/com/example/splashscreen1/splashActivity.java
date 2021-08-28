827bb4d5b82f6dcf7b51bac3daff500ed779b123
sds7500 <somyasheti@gmail.com>
2021-08-29 02:31:37 +0530

    this is my final commit.

---
+++ app/src/main/java/com/example/splashscreen1/splashActivity.java
@@ -0,0 +1,34 @@
+package com.example.splashscreen1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class splashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Thread td=new Thread(){
+            @Override
+            public void run() {
+                try{
+                    sleep(5000);
+                }catch (Exception ex){
+                    ex.printStackTrace();
+                }finally {
+                    Intent intent=new Intent(splashActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };td.start();
+    }
+}
